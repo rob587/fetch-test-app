732402455d208385e52125c4eca33620e20347a8
rob587 <robertocamm02@yahoo.com>
2026-03-31 12:06:59 +0200

    added information with state and ts infos

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+};
+
 export default function Index() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [errore, setErrore] = useState("");
+
+  useEffect(() => {});
   return (
     <View
       style={{
